c8fc4a0b9be56ebdcaa2852315f28749e30d0ee0
Yashwanth N-SVNIT <u24ai118@aid.svnit.ac.in>
2024-09-04 00:07:41 +0530

    Add files via upload

---
+++ Y_Assignment11.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+  int S,hr,min,sec;
+  printf("Enter the time in toatal seconds: ");
+  scanf("%d", &S);
+  hr=S/(60*60);
+  min=(S-(hr*60*60))/60;
+  sec=S-(hr*60*60)-(min*60);
+  printf("The time is %d:%d:%d", hr,min,sec);
+}
