f22c7beb993b309bd7af8409e341db74b0fbb92f
Yashwanth N-SVNIT <u24ai118@aid.svnit.ac.in>
2024-09-04 00:02:23 +0530

    Update and rename Y_Assignment4.c to Assignment04.c

--- Assignment04.c
+++ Assignment04.c
@@ -1,9 +1,10 @@
 #include<stdio.h>
-void main()
+int main()
 {
   float fr,cd;
   printf("Enter the temperature in fahrenheit: ");
   scanf("%f",&fr);
   cd=((fr-32)*5)/9;
   printf("The temperature in degree centigrade is %f", cd);
+  return 0;
 }
