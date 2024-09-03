8abb0a9efe049a531f1165a39bf4e72ec2ddae2c
Yashwanth N-SVNIT <u24ai118@aid.svnit.ac.in>
2024-09-04 00:05:18 +0530

    Update and rename Y_Assignment9.c to Assignment09.c

--- Assignment09.c
+++ Assignment09.c
@@ -1,9 +1,10 @@
 #include<stdio.h>
-void main()
+int main()
 {
   int hr,min,sec,S;
   printf("Enter time in Hours Minutes Seconds: ");
   scanf("%d%d%d", &hr,&min,&sec);
   S=(hr*60*60)+(min*60)+sec;
   printf("The time in seconds is %d", S);
+  return 0;
 }
