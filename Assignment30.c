d7238c26f7c7629b0c9202e28038667934f01837
Yashwanth N-SVNIT <u24ai118@aid.svnit.ac.in>
2024-09-11 09:24:39 +0530

    Assignment30.c

--- Assignment30.c
+++ Assignment30.c
@@ -2,22 +2,15 @@
 #include<math.h>
 int main()
 { 
-  int num,count,temp,i,rev;
+  int num,i,rev;
   printf("Enter the number: ");
   scanf("%d",&num);
-  temp=num,count=0,rev=0;
-  while(temp!=0)
-  {
-   temp/=10;
-   count++;
-  }
-  temp=num;
-  while(count>0)
+  rev=0;
+  while(num!=0)
   { 
-   i=temp%10;
-   rev=rev+i*pow(10,count-1);
-   count--;
-   temp/=10;
+   i=num%10;
+   rev=rev*10+i;
+   num/=10;
   }
   printf("The reverse of the given number is %d", rev );
   return 0;
